35ea40bb2d8b6a844131bad613877db9d99771aa
Victor Azevedo <victorazesc@hotmail.com>
2023-05-12 08:30:07 -0300

    deploy

--- serverless.ts
+++ serverless.ts
@@ -16,7 +16,7 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
-    },
+    }
   },
   // import the function via paths
   functions: { hello },
